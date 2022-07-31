625785e499097f2fa62e3f11112a2d268724fa33
BlackWiind <BlackWiind@gmail.com>
2022-08-01 08:50:41 +1000

    release

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ from math import sin, cos
 c = 5
 h = 0.023
 x = c-h
-derivative = array('d', [])
+derivatives = array('d', [])
 
 
-def calc(_x):
-    return round(1/c * sin(c*_x), 4)
+def calc(var: float) -> float:
+    return round(1 / c * sin(c * var), 4)
 
 
-print("\tТаблица 1:\t")
+print("\t\tТаблица значений функции:\t\t")
+print("\t******************************\t")
 while x < c+16*h:
 
     print("\tx = %.4f" % x, "\t|\tf(x) = %.4f" % calc(x))
-    derivative.append(calc(x+h) - calc(x-h)/(2*h))
+    derivatives.append((calc(x+h) - calc(x-h))/(2*h))
     x += h
 
+print("\t******************************\t\n")
 x = c-h
-print("\tТаблица 2:\t")
-for i in derivative:
-    print("\tx = ", x, "\t|\tf'(x) = %.4f" % derivative[i], "\t|\tf'(x) = %.4f" % cos(x*c))
+print("\t\t\tПриближенные и точные значения функции:\t\t")
+print("\t*********************************************************\t")
+print("\t\t x\t\t|\tприближенное\t|\tточное\t")
+print("\t*********************************************************\t")
+for i in range(derivatives.__len__()):
+    print("\tx = %.4f" % x, "\t|\tf'(x) = %.4f" % derivatives[i], "\t|\tf'(x) = %.4f" % cos(x*c))
     x += h
-    
+print("\t*********************************************************\t")
